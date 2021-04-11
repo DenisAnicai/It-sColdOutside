642757b9b685637fd34d7e0afe7bb87890ef7c68
DESKTOP-4MQQBKS\Denis Anicai <denisanicai14@gmail.com>
2021-04-11 21:05:27 +0300

    First commit

---
+++ It's Cold Outside/Assets/Scripts/sceneManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class sceneManager : MonoBehaviour
+{
+    public Image fadeImage;
+    private GameObject dialogue;
+    public AudioClip[] sound;
+    public AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        dialogue = GameObject.Find("DialogManager");
+        if (SceneManager.GetActiveScene().name == "1")
+        {
+            StartCoroutine(FadeIn(4f));
+            StartCoroutine(StartDialogueWithDelay(0, 7f));
+        }
+        else if (SceneManager.GetActiveScene().name == "6")
+        {
+            StartCoroutine(StartDialogueWithDelay(11, 1.5f));
+        }
+        else if (SceneManager.GetActiveScene().name == "12")
+        {
+            source.clip = sound[0];
+            source.Play();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator StartDialogueWithDelay(int dialogueNumber, float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        dialogue.GetComponent<dialog>().StartDialogueInteraction(dialogueNumber);
+    }
+    public IEnumerator FadeIn(float fadeInTime)
+    {
+        fadeImage.gameObject.SetActive(true);
+        fadeImage.canvasRenderer.SetAlpha(1.0f);
+
+        fadeImage.CrossFadeAlpha(0, fadeInTime, false);
+        yield return new WaitForSeconds(fadeInTime);
+        
+
+    }
+    public IEnumerator FadeOut(float fadeOutTime)
+    {
+        fadeImage.gameObject.SetActive(true);
+        fadeImage.canvasRenderer.SetAlpha(0.0f);
+
+        fadeImage.CrossFadeAlpha(1, fadeOutTime, false);
+        yield return new WaitForSeconds(fadeOutTime);
+
+
+    }
+    public IEnumerator Fade(float fadeInTime, float fadeOutTime)
+    {
+        fadeImage.gameObject.SetActive(true);
+        fadeImage.CrossFadeAlpha(1, fadeInTime, false);
+        yield return new WaitForSeconds(fadeInTime);
+        fadeImage.CrossFadeAlpha(0, fadeOutTime, false);
+    }
+
+}
---
+++ It's Cold Outside/It's Cold Outside/Assets/Scripts/moveCharacter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class moveCharacter : MonoBehaviour
+{
+    
+    private Rigidbody2D rb;
+    public float speed = 1f;
+    public Animator animator;
+    private Vector2 moveVelocity;
+    GameObject dialogue, sceneBoss;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneBoss = GameObject.Find("sceneBoss");
+        dialogue = GameObject.Find("DialogManager");
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(dialogue.GetComponent<dialog>().GetPermissionToMove())
+        {
+            if(dialogue.GetComponent<dialog>().previousScene == 24)
+            {
+                StartCoroutine(LoadNextScene("16"));
+            }
+            animator.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
+            if (animator.GetFloat("Horizontal") != 0) animator.SetBool("WalkSide", true);
+            else animator.SetBool("WalkSide", false);
+            animator.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
+            Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            moveVelocity = moveInput.normalized * speed;
+        }
+    }
+    public IEnumerator LoadNextScene(string scene)
+    {
+        sceneBoss.GetComponent<sceneManager>().StartCoroutine(sceneBoss.GetComponent<sceneManager>().FadeOut(1.5f));
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(scene);
+    }
+    private void FixedUpdate()
+    {
+        if (dialogue.GetComponent<dialog>().GetPermissionToMove())
+            rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
